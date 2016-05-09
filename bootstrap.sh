d85d3257bbfaad5742afd858c9a1cdac22bf9aa8
Tyson-Skiba-Uni <tyson.skiba@live.vu.edu.au>
2016-05-09 13:28:36 +1000

    Commit 2: Bootstrap Done

---
+++ bootstrap.sh
@@ -0,0 +1,5 @@
+#!/bin/bash                       
+#Standard for a Shell Script Says its Designed for Bash
+#Must be made Executable "chmod u+x ./bootstrap.sh"
+
+autoreconf --install              #Run This Command

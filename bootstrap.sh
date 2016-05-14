6edc97b0e836920387ded1efb263ea72e889edbd
Tyson-Skiba-Uni <tyson.skiba@live.vu.edu.au>
2016-05-15 02:53:39 +1000

    Commit 8: Bootsrap Aesthetics

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,21 @@
 
 autoreconf --install              #Run This Command
 ./configure
-make
+if make; then
+	printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+	COLUMNS=$(tput cols) 
+	bold=$(tput bold )
+	title=" ${bold}EES4100: Operating Systems and Networking" 
+	printf "%*s\n" $(((${#title}+$COLUMNS)/2)) "$title"
+	printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+	normal=$(tput sgr0)
+	title="${normal}Bridge Has Been Localy Built." 
+	printf "%*s\n" $(((${#title}+$COLUMNS)/2)) "$title"
+	title="To Install Run 'make install' as root" 
+	printf "%*s\n" $(((${#title}+$COLUMNS)/2)) "$title"
+	title="Alternatively, You May Now Execute the Program by typing './src/bridge'" 
+	printf "%*s\n" $(((${#title}+$COLUMNS)/2)) "$title"
+	printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+else
+    echo "Command failed"
+fi
